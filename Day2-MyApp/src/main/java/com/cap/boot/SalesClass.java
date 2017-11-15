b9932cff5390f95f54f6edb4e8a0b592f225e212
lnc <lnc@DIN76000832.corp.capgemini.com>
2017-11-15 12:22:00 +0530

    updated

--- Day2-MyApp/src/main/java/com/cap/boot/SalesClass.java
+++ Day2-MyApp/src/main/java/com/cap/boot/SalesClass.java
@@ -3,11 +3,15 @@ package com.cap.boot;
 public class SalesClass {
 	int[] arr={1,2,3,4,5};
 	
+	
 	public void display(){
-		
+		int sum=0;
 		System.out.println("Display Method");
-		for(int i=0;i<arr.length;i++)
+		for(int i=0;i<arr.length;i++){
 			System.out.println(arr[i]);
+		sum+=arr[i];
+		}
+		System.out.println("the sum value:" +sum);
 	}
-
+		
 }
